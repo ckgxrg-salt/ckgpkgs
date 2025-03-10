f17d294164c05f87dd9e01122b0a4c68ed9cee71
ckgxrg <ckgxrg@ckgxrg.io>
2025-03-10 18:13:22 +0800

    Add ch57x-keyboard-tool

--- packages/catalog.nix
+++ packages/catalog.nix
@@ -7,7 +7,6 @@ with pkgs;
   nu-scripts = callPackage ./theming/nu-scripts.nix { };
   vivid-icons = callPackage ./theming/vivid-icons.nix { };
   wvkbd-desktop = callPackage ./wvkbd-desktop.nix { };
-  fyrox-template = callPackage ./fyrox-template.nix { };
   rofi-themes = callPackage ./theming/rofi-themes.nix { };
   maple-ui = callPackage ./theming/maple-ui.nix { };
 
@@ -17,6 +16,8 @@ with pkgs;
   space-gtk-theme = callPackage ./theming/space-gtk.nix { };
   marge-kvantum-theme = qt6Packages.callPackage ./theming/marge-kvantum.nix { };
 
-  # My personal packages
+  # Misc
+  ch57x-keyboard-tool = callPackage ./ch57x-keyboard-tool.nix { };
+  fyrox-template = callPackage ./fyrox-template.nix { };
   lxwengd = callPackage ./lxwengd.nix { };
 }
---
+++ packages/ch57x-keyboard-tool.nix
@@ -0,0 +1,13 @@
+{ rustPlatform, fetchCrate }:
+# Driver for mini keyboard
+rustPlatform.buildRustPackage rec {
+  pname = "ch57x-keyboard-tool";
+  version = "1.5.2";
+
+  src = fetchCrate {
+    inherit pname version;
+    sha256 = "sha256-HldGw7m32PZUuDQIpYCdauk6B75sD0S5I8NNbpT7X4A=";
+  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-/I77kgC596jE6PDGzF1MjD6tyj880hcHEL4HSuFHaew=";
+}
