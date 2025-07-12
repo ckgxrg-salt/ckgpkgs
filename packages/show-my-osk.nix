69c90b5d45de2e1e97c4ca09e7c45f7ddee10af9
ckgxrg <ckgxrg@ckgxrg.io>
2025-07-12 22:47:15 +0800

    show-my-osk: update

--- packages/show-my-osk.nix
+++ packages/show-my-osk.nix
@@ -10,16 +10,16 @@ hyprlandPlugins.mkHyprlandPlugin hyprland {
 
   src = fetchFromGitHub {
     owner = "ckgxrg-salt";
-    repo = "ShowMyOSK";
-    rev = "7f0687f1d1e8851898c16971cf8061ef586853cf";
-    hash = "sha256-rzud5hucG3LmRABU5/fMoy1dgx+zWykRULSvoPrT0OE=";
+    repo = "show-my-osk";
+    rev = "bd6a59ca000121781e44446666693c99060ee03b";
+    hash = "sha256-q+sathe7UInyCi0ROBLbymO3Mq1EcfStwL82MurKywk=";
   };
 
   inherit (hyprland) nativeBuildInputs;
 
   meta = with lib; {
-    homepage = "https://github.com/ckgxrg-salt/ShowMyOSK";
-    description = "Automatically pops up on-screen keyboard when an input field is focused for Hyprland.";
+    homepage = "https://github.com/ckgxrg-salt/show-my-osk";
+    description = "A Hyprland plugin that automatically pops up on-screen keyboard upon focusing an input field.";
     license = licenses.bsd2;
     platforms = platforms.linux;
   };
